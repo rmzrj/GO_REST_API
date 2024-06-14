267901609ce868e50dd69ee415d394cd1c8c186f
ramees <rameesraj534@gmail.com>
2024-06-14 17:42:39 +0530

    feat: GET, POST event handling added

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"rest_api_example.com/models"
 )
 
 func main() {
@@ -11,12 +12,31 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 
 }
 
-func getEvents (context *gin.Context){
-	context.JSON(http.StatusOK, gin.H{"message": "Aree bhaauiii"})
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
 
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data !!!!"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "event created!!!", "event": event})
 }
--- models/event.go
+++ models/event.go
@@ -4,10 +4,10 @@ import "time"
 
 type Event struct {
 	ID          int
-	Name        string
-	Description string
-	Location    string
-	DateTime    time.Time
+	Name        string    `binding:required`
+	Description string    `binding:required`
+	Location    string    `binding:required`
+	DateTime    time.Time `binding:required`
 	UserID      int
 }
 
@@ -19,4 +19,6 @@ func (e Event) Save() {
 
 }
 
-
+func GetAllEvents() []Event {
+	return events
+}
