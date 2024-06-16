135ede9fcd89188af47822450b95cd6df05c68c4
ramees <rameesraj534@gmail.com>
2024-06-16 19:12:09 +0530

    password hashing added

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "rest_api_example.com/db"
+import (
+	"rest_api_example.com/db"
+	"rest_api_example.com/utils"
+)
 
 type User struct {
 	ID       int64
@@ -16,7 +19,12 @@ func (u User) Save() error {
 	}
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
 
 	if err != nil {
 		return err
